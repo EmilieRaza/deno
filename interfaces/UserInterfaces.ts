3838055689078dfe0b28afd649e7bd44681f48d3
Emilie <razafiemilie@hotmail.fr>
2020-12-15 10:33:52 +0100

    Add files via upload

---
+++ interfaces/UserInterfaces.ts
@@ -0,0 +1,21 @@
+import { roleTypes } from '../types/roleTypes.ts';
+
+export default interface UserInterfaces {
+
+    _id: { $oid: string }|null;
+
+    email: string;
+    password: string;
+    lastname: string;
+    firstname: string;
+    phoneNumber ? : string;
+
+    dateNaiss: Date;
+    role: roleTypes;
+
+    getAge(): Number;
+    fullName(): string;
+    insert(): Promise<void> ;
+    update(): Promise < any > ;
+    delete(): Promise < any > ;
+}
